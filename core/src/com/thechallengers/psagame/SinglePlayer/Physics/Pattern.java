53dec8d99219e8ada1c1da0b10675117efbb819a
Le Trung Hieu <lth08091998@gmail.com>
2017-09-29 22:54:56 +0800

    Read image and parse result

--- core/src/com/thechallengers/psagame/SinglePlayer/Physics/Pattern.java
+++ core/src/com/thechallengers/psagame/SinglePlayer/Physics/Pattern.java
@@ -1,5 +1,9 @@
 package com.thechallengers.psagame.SinglePlayer.Physics;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+
 /**
  * Created by Asus on 9/29/2017.
  */
@@ -27,10 +31,16 @@ public class Pattern {
     }
 
     public static void renderTriangle(boolean[][] mat, int size) {
-        for (int i=0; i<size; i++) {
-            for (int j=0; j<i; j++) {
-                mat[i][j] = true;
+        Texture a = new Texture(Gdx.files.internal("textures/triangle.png"));
+        a.getTextureData().prepare();
+        Pixmap pixmap = a.getTextureData().consumePixmap();
+        for (int i=0; i<100; i++) {
+            for (int j=0; j<100; j++) {
+                if(pixmap.getPixel(i, j) != 0) {
+                    mat[i][j] = true;
+                }
             }
         }
+
     }
 }
