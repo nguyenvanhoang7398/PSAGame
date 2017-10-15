949216cfa294eccb9bc572c40d7d6c0abf1f67e6
NguyenVanHoang <nguyenvanhoang7398@gmail.com>
2017-10-15 12:53:38 +0800

    Adjust random controller

--- core/src/com/thechallengers/psagame/SinglePlayer/Physics/RandomController.java
+++ core/src/com/thechallengers/psagame/SinglePlayer/Physics/RandomController.java
@@ -8,9 +8,15 @@ import java.util.*;
 
 public class RandomController {
     public static Random r = new Random();
+    public static double WIDTH_3_PERCENTAGE = 0.7;
 
     public static Block randomBlock() {
         Block block = new Block(r.nextInt(5) + 1, r.nextInt(3) + 1, r.nextInt(2) + 1);
+
+        while (block.width != 3 && !(r.nextDouble() <= WIDTH_3_PERCENTAGE)) {
+            block = new Block(r.nextInt(5) + 1, r.nextInt(3) + 1, r.nextInt(2) + 1);
+        }
+
         return block;
     }
 }
